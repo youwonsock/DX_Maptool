059573df24330d34b18c8c04d5eacfbe295afd93
youwonsock <46276141+youwonsock@users.noreply.github.com>
2023-11-29 14:55:19 +0900

    add Assimp lib

--- DirectX Project/ProjectTemplate/NormalMappingDemo.cpp
+++ DirectX Project/ProjectTemplate/NormalMappingDemo.cpp
@@ -7,24 +7,46 @@ void NormalMappingDemo::Init()
 {
 	shader = std::make_shared<Shader>(L"14. NormalMapping.fx"); 
 
-	{
+	{	// old lemon chim
 		std::shared_ptr<Material> material = std::make_shared<Material>();
 		{
 			material->SetShader(shader);
 		}
 		{
-			auto textue = ResourceManager::GetInstance().Load<Texture>(L"LemonChim", L"IMG/Leather.jpg" );
+			auto textue = ResourceManager::GetInstance().Load<Texture>(L"OldLemonChim", L"IMG/OldGreenLemonChim.png" );
 			material->SetDiffuseMap(textue);
 		}
 		{
-			auto textue = ResourceManager::GetInstance().Load<Texture>(L"LeatherNormal", L"IMG/Leather_Normal.jpg");
+			auto textue = ResourceManager::GetInstance().Load<Texture>(L"OldLemonNormal", L"IMG/OldGreenLemonChimNormal.png");
 			material->SetNormalMap(textue);
 		}
 		MaterialDesc& materialDesc = material->GetMaterialDesc();
 		materialDesc.ambient = Color(1.f, 1.0f, 1.0f, 1.0f);
 		materialDesc.diffuse = Color(1.0f, 1.0f, 1.0f, 1.0f);
 		materialDesc.specular = Color(1.0f, 1.0f, 1.0f, 1.0f);
-		materialDesc.emissive = Vector4(1.0f);
+		materialDesc.emissive = Vector4(0.5f);
+
+		ResourceManager::GetInstance().Add(L"OldLemonChimMat", material);
+	}
+
+	{	// lemon chim
+		std::shared_ptr<Material> material = std::make_shared<Material>();
+		{
+			material->SetShader(shader);
+		}
+		{
+			auto textue = ResourceManager::GetInstance().Load<Texture>(L"LemonChim", L"IMG/YellowLemonChim.png");
+			material->SetDiffuseMap(textue);
+		}
+		{
+			auto textue = ResourceManager::GetInstance().Load<Texture>(L"LemonNormal", L"IMG/LemonChimNormal.png");
+			material->SetNormalMap(textue);
+		}
+		MaterialDesc& materialDesc = material->GetMaterialDesc();
+		materialDesc.ambient = Color(1.f, 1.0f, 1.0f, 1.0f);
+		materialDesc.diffuse = Color(1.0f, 1.0f, 1.0f, 1.0f);
+		materialDesc.specular = Color(1.0f, 1.0f, 1.0f, 1.0f);
+		materialDesc.emissive = Vector4(0.5f);
 
 		ResourceManager::GetInstance().Add(L"LemonChimMat", material);
 	}
@@ -38,7 +60,7 @@ void NormalMappingDemo::Init()
 		obj1->GetMeshRenderer()->SetMesh(mesh);
 	}
 	{
-		auto material = ResourceManager::GetInstance().Get<Material>(L"LemonChimMat");
+		auto material = ResourceManager::GetInstance().Get<Material>(L"OldLemonChimMat");
 		obj1->GetMeshRenderer()->SetMaterial(material);
 	}
 
@@ -51,10 +73,9 @@ void NormalMappingDemo::Init()
 		obj2->GetMeshRenderer()->SetMesh(mesh);
 	}
 	{
-		auto material = ResourceManager::GetInstance().Get<Material>(L"LemonChimMat")->Clone();
+		auto material = ResourceManager::GetInstance().Get<Material>(L"LemonChimMat");
 		obj2->GetMeshRenderer()->SetMaterial(material);
 	}
-
 
 	// camera
 	cameraObject = std::make_shared<GameObject>();
@@ -83,6 +104,8 @@ void NormalMappingDemo::PreRender()
 {
 }
 
+Vector3 rotate1 = Vector3(0,0,0);
+Vector3 rotate2 = Vector3(0,0,0);
 void NormalMappingDemo::Render()
 {
 	Global::g_immediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
@@ -90,10 +113,10 @@ void NormalMappingDemo::Render()
 	// set global light
 	{
 		LightDesc lightDesc;
-		lightDesc.ambient = Vector4(0.f);
-		lightDesc.diffuse = Vector4(0.f);
-		lightDesc.specular = Vector4(0.f);
-		lightDesc.emissive = Vector4(1.0f, 0.0f, 0.0f,1.f);
+		lightDesc.ambient = Vector4(1.f);
+		lightDesc.diffuse = Vector4(1.f);
+		lightDesc.specular = Vector4(1.f);
+		lightDesc.emissive = Vector4(1.0f);
 		lightDesc.direction = Vector3(1.0f, 0.f, 1.0f);
 
 		RenderManager::GetInstance().PushLightData(lightDesc);
@@ -101,9 +124,14 @@ void NormalMappingDemo::Render()
 
 	// set obj material
 	{
+		rotate1.y += 0.0001f;
+		obj1->GetTransform()->SetWorldRotation(rotate1);
 		obj1->Update();
 	} 
 	{
+		rotate2.y += 0.0001f;
+		rotate2.z += 0.00005f;
+		obj2->GetTransform()->SetWorldRotation(rotate2);
 		obj2->Update();
 	}
 }
